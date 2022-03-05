085de1b0d6cd326fb0ea765a5b28c8459aca3632
Ann-Marie <ann.marie.amr@gmail.com>
2022-03-05 18:55:34 +0100

    Battle class and tests classes created

---
+++ src/main/java/Units/Battle.java
@@ -0,0 +1,19 @@
+package Units;
+
+public class Battle {
+    private Army armyOne;
+    private Army armyTwo;
+
+    public void Battle(Army armyOne, Army armyTwo){
+        //TODO
+    }
+
+    public Army simulate(){
+        //TODO
+    }
+
+    @Override
+    public String toString(){
+        //TODO
+    }
+}
